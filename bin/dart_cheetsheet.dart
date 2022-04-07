a65b83d16be463e39e46dca98fe7a88e83097124
Omkar Chendwankar <omkarchendwankar45@gmail.com>
2022-04-07 11:49:40 +0530

    Creating a new class cat

--- bin/dart_cheetsheet.dart
+++ bin/dart_cheetsheet.dart
@@ -1,3 +1,18 @@
-void main(){
-  
+import 'dart:developer';
+import 'dart:io';
+
+void main() {
+  var cat = Cat();
+  cat.run();
+}
+
+abstract class CanRun {
+  void run();
+}
+
+class Cat extends CanRun {
+  @override
+  void run() {
+    stdout.write("This is a Cat which inherits from class CanRun");
+  }
 }
