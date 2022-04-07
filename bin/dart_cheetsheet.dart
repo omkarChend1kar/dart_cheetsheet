1e1184dc6204b7edf1cebac1c10f72add6cc6f22
Omkar Chendwankar <omkarchendwankar45@gmail.com>
2022-04-07 12:05:43 +0530

    Adding Constructor to abstract class

--- bin/dart_cheetsheet.dart
+++ bin/dart_cheetsheet.dart
@@ -6,13 +6,18 @@ void main() {
   cat.run();
 }
 
+enum Type { Cat, Dog, Cow, Kangaroo }
+
 abstract class CanRun {
+  final Type type;
+  const CanRun({required this.type});
   void run() {
     stdout.writeln("This is CanRun abstract class");
   }
 }
 
 class Cat extends CanRun {
+  const Cat() : super(type: Type.Cat);
   @override
   void run() {
     super.run();
