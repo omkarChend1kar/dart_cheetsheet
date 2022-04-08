3a725a9952555e087d36edf397bf4f686e080e8d
Omkar Chendwankar <omkarchendwankar45@gmail.com>
2022-04-08 13:41:39 +0530

    Sound Null Safety

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-A simple command-line application.
+Sound Null Safety:
+
+How to make object nullable?
+
+By adding ex. List<String?>? which means List itself can be nullable with its individual elements also.
+
+Picking non-nullable values with ?? operator.
+
+Null-aware assignment with ??= operator
+ex. String ? name; If name is null then assign it to string 'nonnull' :Syntax= name??='nonnull  '; otherwise don't assign it anything
+
+Syntax to conditionally invoke a method or property using ?.(methods or properties are invoked only if value in Non-null)
+
+
+
+
+
+
+
--- bin/dart_cheetsheet.dart
+++ bin/dart_cheetsheet.dart
@@ -1,3 +1,20 @@
-void main(List<String> arguments) {
-  print('Hello Server');
+void main() {
+  test();
+}
+void test() {
+  //making List of names nullable.
+  List<String?>? firstName;
+  List<String?>? middleName;
+  List<String?>? lasttName;
+  //Assigning Non-null value if var has null value;
+  /*firstName ??=   ['Omkar'];*/
+  lasttName ??= ['Chendwankar'];
+  // middleName ??= ['Vinayak'];
+  //storing a first non-null value;
+  List<String?>? firstNonNullableName = firstName ?? middleName ?? lasttName;
+  print(firstNonNullableName);
+
+  //Invoking methods conditionally(Condintion being not null)
+  final lengthofList = firstName?.length ?? 0;
+  print(lengthofList);
 }
